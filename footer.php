6861a6ec1819c63bad93f64c47688340269f978f
carlfmichel <carlfmichel@gmail.com>
2016-07-05 06:24:59 -0400

    Added Footer

--- footer.php
+++ footer.php
@@ -1,2 +1,11 @@
+	<footer>
+		<p class="copyright">&copy;<?php echo date("Y"); ?> Rivercity Flooring. All Rights Reserved.</p>
+		<div class="top-links">
+			<a class="top-link" href="#">Home</a>
+			<a class="top-link" href="#">About Us</a>
+			<a class="top-link" href="#">Contact Us</a>
+		</div>
+		<a class="phone" href="tel:5022618233">502.261.8233</a>
+	</footer><!-- end .footer -->
 </body>
 </html>
