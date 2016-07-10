28946914c4904fb3d68ad0897111616b30dd6660
Ty Goss <tygoss@gmail.com>
2016-07-10 12:43:38 -0400

    home page content and divider

--- footer.php
+++ footer.php
@@ -1,4 +1,27 @@
-	<footer>
+<div class="margin three-column-cta">
+	<div class="container">
+		<a class="box box-tan box-1" href="#">
+			<span class="sub-text">Something Here</span>
+			<h2 class="main-title">This is a title box</h2>
+			<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.</p>
+			<img src="images/arrow-right.svg"/>
+		</a>
+		<a class="box box-dark-tan box-2" href="#">
+			<span class="sub-text">Something Here</span>
+			<h2 class="main-title">This is a title box</h2>
+			<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.</p>
+			<img src="images/arrow-right.svg"/>
+		</a>
+		<a class="box box-red box-3" href="#">
+			<span class="sub-text">Something Here</span>
+			<h2 class="main-title">This is a title box</h2>
+			<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.</p>
+			<img src="images/arrow-right.svg"/>
+		</a>
+	</div>
+</div><!-- end .three-column-cta -->
+
+    <footer>
 		<p class="copyright">&copy;<?php echo date("Y"); ?> Rivercity Flooring. All Rights Reserved.</p>
 		<div class="top-links">
 			<a class="top-link" href="#">Home</a>
--- home.php
+++ home.php
@@ -2,4 +2,251 @@
 
 
 
+
+
+<!-- HOME BANNER -->
+
+<div class="margin banner">
+    <a href="" class="banner-main" style="background-image:url(images/cat.png)">
+    
+        <div class="banner-main-info">
+        
+            <h1>Come see our new<br /><strong>Pre-Finished Hardwoods</strong></h1>
+
+            <p>We have a wide range of colors and species, including those by Bruce, Armstrong, Johnson, Anderson and Bella Cera.</p>
+            
+        </div>
+        
+    </a>
+    <a href="" class="banner-top brown">
+        
+        <div class="banner-side-info">
+    
+            <h2><strong>Free</strong> Estimate</h2>
+
+            <p>Ready to get started? Contact us now for a free quote.</p>
+            
+        </div>
+        
+        <img class="arrow" src="images/arrow.svg" />
+        
+    </a>
+    <a href="" class="banner-bottom burgundy">
+        
+        <div class="banner-side-info">
+    
+            <h2>Current <strong>Specials</strong></h2>
+
+            <p>Warehouse Specials $2.99 to $4.99 Solids, Engineered, Waterproof Floors</p>
+            
+        </div>
+
+        <img class="arrow" src="images/arrow.svg" />
+        
+    </a>
+</div>
+
+
+
+<!-- OPEN CONTENT -->
+
+
+<div class="margin open-content">
+    
+    <div class="text">
+    
+        <h1>Louisville’s Complete <strong>Flooring Specialists</strong></h1>
+        
+        <p>Since 1997, RiverCity Flooring has been a trusted name in Louisville for hardwood floors. We have expanded our product line to include carpet,  luxury vinyl tile and laminate.</p>
+
+        <p>We are widely recognized for our pre-finished hardwoods in a wide range of colors and species, including those by Bruce, Armstrong, Johnson, Anderson and Bella Cera.</p>
+
+    </div>
+    
+</div>
+
+
+<div class="divider"><img src="images/divider.jpg" /></div>
+
+
+<!-- FEATURED CATEGORY -->
+<div class="margin fc">
+    <a href="" class="fc-name" style="background-image:url(images/cat.png);">
+        <span>SHOP ALL<strong>HARDWOOD</strong></span>
+        <img class="arrow" src="images/arrow.svg" />
+    </a>
+    <div class="fc-products">
+        <a href="" class="fc-product">
+            <strong>Natural Red</strong>
+            <img class="fc-product-image" src="images/wood1.png" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Gunstock</strong>
+            <img class="fc-product-image" src="images/wood2.png" />
+            <img class="stock" src="images/stock.svg" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Natural White</strong>
+            <img class="fc-product-image" src="images/wood3.png" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Shadow</strong>
+            <img class="fc-product-image" src="images/wood4.png" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Butterscotch</strong>
+            <img class="fc-product-image" src="images/wood5.png" />
+            <img class="stock" src="images/stock.svg" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Cherry Oak</strong>
+            <img class="fc-product-image" src="images/wood6.png" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Chestnut</strong>
+            <img class="fc-product-image" src="images/wood7.png" />
+            <img class="stock" src="images/stock.svg" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Maple Pine</strong>
+            <img class="fc-product-image" src="images/wood8.png" />
+        </a>
+        <div style="clear:both"></div>
+    </div>
+</div>
+
+
+
+
+
+
+<!-- FEATURED CATEGORY -->
+<div class="margin fc">
+    <a href="" class="fc-name" style="background-image:url(images/carpet.jpg);">
+        <span>SHOP ALL<strong>CARPET</strong></span>
+        <img class="arrow" src="images/arrow.svg" />
+    </a>
+    <div class="fc-products">
+        <a href="" class="fc-product">
+            <strong>Vanilla Creamer</strong>
+            <img class="fc-product-image" src="images/carpet1.png" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Sweet Innocence</strong>
+            <img class="fc-product-image" src="images/carpet2.png" />
+            <img class="stock" src="images/stock.svg" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Botanical</strong>
+            <img class="fc-product-image" src="images/carpet3.png" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Sweet Nectar</strong>
+            <img class="fc-product-image" src="images/carpet4.png" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Moonbeam</strong>
+            <img class="fc-product-image" src="images/carpet5.png" />
+            <img class="stock" src="images/stock.svg" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Soft Sky</strong>
+            <img class="fc-product-image" src="images/carpet6.png" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Burnt Copper</strong>
+            <img class="fc-product-image" src="images/carpet7.png" />
+            <img class="stock" src="images/stock.svg" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Frosted Berry</strong>
+            <img class="fc-product-image" src="images/carpet8.png" />
+        </a>
+        <div style="clear:both"></div>
+    </div>
+</div>
+
+
+<!-- FEATURED CATEGORY -->
+<div class="margin fc">
+    <a href="" class="fc-name" style="background-image:url(images/laminate.jpg);">
+        <span>SHOP ALL<strong>LAMINATE</strong></span>
+        <img class="arrow" src="images/arrow.svg" />
+    </a>
+    <div class="fc-products">
+        <a href="" class="fc-product">
+            <strong>Molassas Maple</strong>
+            <img class="fc-product-image" src="images/laminate1.png" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Antique Cream Pine</strong>
+            <img class="fc-product-image" src="images/laminate2.png" />
+            <img class="stock" src="images/stock.svg" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Hickory Shadow Oak</strong>
+            <img class="fc-product-image" src="images/laminate3.png" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Havannah Hickory</strong>
+            <img class="fc-product-image" src="images/laminate4.png" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Smokehouse Oak</strong>
+            <img class="fc-product-image" src="images/laminate5.png" />
+            <img class="stock" src="images/stock.svg" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Beechwood Cream</strong>
+            <img class="fc-product-image" src="images/laminate6.png" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Warm Autumn Oak</strong>
+            <img class="fc-product-image" src="images/laminate7.png" />
+            <img class="stock" src="images/stock.svg" />
+        </a>
+        <a href="" class="fc-product">
+            <strong>Fawn Chestnut</strong>
+            <img class="fc-product-image" src="images/laminate8.png" />
+        </a>
+        <div style="clear:both"></div>
+    </div>
+</div>
+
+
+
+
+
+<div class="divider"><img src="images/divider.jpg" /></div>
+
+
+
+
+<!-- SPLIT CONTENT -->
+
+<div class="margin split-content">
+
+    <div class="left text lightbrown">
+    
+        <h2>Louisville’s <strong>most extensive flooring resource</strong></h2>
+        
+        <p>Savvy homeowners know we’re Louisville’s most extensive flooring resource, but they also rely on us to <a href="">sand and finish hardwood floors.</a></p>
+        
+        <p>We serve Louisville Metro and surrounding areas. We invite you to stop by our showroom to meet with our experts and check out our vast selection. Or, simply fill out the <a href="">Contact Us</a> form, and we’ll come to your home for a <a href="">FREE estimate</a>.</p>
+                
+    </div>
+    
+    <div class="right image" style="background-image:url(images/cat.png);">
+    
+    </div>
+    
+</div>
+
+
+
+<div class="divider"><img src="images/divider.jpg" /></div>
+
+
+
+
 <?php include('footer.php'); ?>
