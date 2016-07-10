2261d80566533c820a3f4c96bc304e4059e7778e
carlfmichel <carlfmichel@gmail.com>
2016-07-10 13:56:14 -0400

    Started sub category

---
+++ sub-category.php
@@ -0,0 +1,12 @@
+<?php include('header.php'); ?>
+
+<div class="margin split-content">
+	<span class="left text lightbrown">
+	<h2><strong>Solid</strong></h2>
+	<p>This type of flooring is a solid piece of wood from top to bottom. The thickness can vary, but generally ranges from 3/4″ to 5/16″ .  One of the many benefits of solid wood flooring is that it can be sanded and refinished numerous times through the years, adding great value to your home at resale.</p>
+	<p>A prospective buyer may appreciate being able to change the color or finish. Another important feature of solid floors is that they can be installed above or on grade. Solid wood floors are available as unfinished or pre-finished.</p>
+	</span>
+	<div class="right image" style="background-image:url(images/cat.png);"></div>
+</div>
+
+<?php include('footer.php'); ?>
