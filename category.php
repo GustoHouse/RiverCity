34ac9ca854d078d2773a4c87ba5508db852cb3ab
carlfmichel <carlfmichel@gmail.com>
2016-07-10 12:47:54 -0400

    Added Logo grid and category stuff

--- category.php
+++ category.php
@@ -1,5 +1,65 @@
 <?php include('header.php'); ?>
 
+<div class="margin cat-runner" style="background-image:url(images/three-column-cta-bg.jpg);">
+	
+</div>
 
+<div class="margin open-content">
+	<div class="text">
+		<h1>Hardwood <strong>Flooring</strong></h1>
+		<p>RiverCity Flooring’s star-studded hardwood flooring collection showcases Armstrong, Anderson, Johnson, Bella Cera, Mannington, Vallaria and Mullican, in addition to other top quality products. Walk into our showroom and imagine our oak, maple, walnut, cherry, birch and mahogany perfectly at home on your floors. But get ready to be surprised by the colors and graining of species such as acacia, tiger wood, merbau and timborana.</p>
+		<p>Running the gamut from rustic to sophisticated, today’s flooring is full of the unexpected.  What’s fresh and new? Wider planks and random widths are hitting the pages of home design magazines. But traditional floors live on in spectacular renditions.</p>
+		<p>Advances in design and manufacturing now enable wood flooring to be installed anywhere in your home or business. However, the location will determine the type of wood flooring that should be used. The professionals at RiverCity Flooring can help you determine the most suitable wood for your application. Here are some guidelines to keep in mind.</p>
+	</div>
+</div>
+
+<div class="margin split-content">
+	<a href="" class="left text lightbrown">
+	<h2><strong>Solid</strong></h2>
+	<p>This type of flooring is a solid piece of wood from top to bottom. The thickness can vary, but generally ranges from 3/4″ to 5/16″ .  One of the many benefits of solid wood flooring is that it can be sanded and refinished numerous times through the years, adding great value to your home at resale.</p>
+	<p>A prospective buyer may appreciate being able to change the color or finish. Another important feature of solid floors is that they can be installed above or on grade. Solid wood floors are available as unfinished or pre-finished.</p>
+	<img class="arrow" src="images/arrow.svg" />
+	</a>
+	<div class="right image" style="background-image:url(images/cat.png);"></div>
+</div>
+
+<div class="margin split-content">
+	<a href="" class="left text brown">
+	<h2><strong>Pre-Finished</strong></h2>
+	<p>Pre-finished hardwood floors, generally engineered, are pre-finished with a color and sheen at the factory.  They do not require sanding or finishing. Many of these are “glue down” or “floating” floors, constructed to click together for easy installation.  Some of our most popular brands of pre-finished floors are Bruce, Armstrong, Dasso, Mohawk, Mullican and Somerset.</p>
+	<img class="arrow" src="images/arrow.svg" />
+	</a>
+	<div class="right image" style="background-image:url(images/cat.png);"></div>
+</div>
+
+<div class="margin split-content">
+	<a href="" class="left text yellow">
+		<h2><strong>Unfinished</strong></h2>
+		<p>Unfinished floors are sanded and finished on-site following installation. They are most often solid wood.</p>
+		<img class="arrow" src="images/arrow.svg" />
+	</a>
+	<div class="right image" style="background-image:url(images/cat.png);"></div>
+</div>
+
+<div class="margin split-content">
+	<a href="" class="left text burgundy">
+		<h2><strong>Engineered</strong></h2>
+		<p>Engineered wood floors are real wood floors that are manufactured using three to nine layers of different wood veneers. The sub layers can be of the same or different species. The top layer consists of high-quality wood. The grain of each layer runs in different directions, which makes the floor very stable. This means the wood will expand and contract less than solid wood flooring during fluctuations in humidity and temperature. Engineered floors are especially appropriate for Louisville’s varying temperatures and humidity.</p>
+		<p>While this type of floor can be sanded and refinished, the process cannot be done as many times as it can with solid wood flooring. The number of times it can be sanded and refinishes depends on the thickness of the top layer. Engineered wood flooring is glued to the sub-floor or concrete and can be installed above, on, or below grade.</p>
+		<img class="arrow" src="images/arrow.svg" />
+	</a>
+	<div class="right image" style="background-image:url(images/cat.png);"></div>
+</div>
+
+<div class="margin logo-grid">
+	<span class="title">Our Selected<strong>Hardwood Partners</strong></span>
+	<a href="#" target="_blank"><img src="images/armstrong.gif"/></a>
+	<a href="#" target="_blank"><img src="images/bruce.jpg"/></a>
+	<a href="#" target="_blank"><img src="images/HomeLegend.jpg"/></a>
+	<a href="#" target="_blank"><img src="images/manninton.jpg"/></a>
+	<a href="#" target="_blank"><img src="images/pergo.jpg"/></a>
+	<a href="#" target="_blank"><img src="images/somerset.jpg"/></a>
+	<a href="#" target="_blank"><img src="images/vallaria.png"/></a>
+</div>
 
 <?php include('footer.php'); ?>
